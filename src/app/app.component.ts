cdd5426fe1fbb9622d2b9b7ff001f657bff4c8ef
Daniel Adones Garcia <daniel.adones@eviden.com>
2024-11-27 08:57:03 +0100

    Decoro la calculadora con bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'MiniCalculadoraApp';
+  title = 'Mini Calculadora';
+  num1:number=0;
+  num2:number=0;
+  result:number=0;
+
+  sumar():void{
+    this.result = this.num1 + this.num2;
+  }
+
+  resta():void{
+    this.result = this.num1 - this.num2;
+  }
+
+  multiplicar():void{
+    this.result = this.num1 * this.num2;
+  }
+
+  dividir():void{
+    if (this.num2 != 0) {
+      this.result = this.num1/this.num2;
+    } else {
+      alert("No se puede dividir por 0 ");
+    }
+    
+  }
+
+
+
+
 }
