edcf19e8646882c6f1d440fe96514d8614adeff5
Daniel Adones Garcia <daniel.adones@eviden.com>
2024-11-28 09:59:43 +0100

    Añado ngif

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,22 +11,27 @@ export class AppComponent {
   num1:number=0;
   num2:number=0;
   result:number=0;
+  calculado =false;
 
   sumar():void{
     this.result = this.num1 + this.num2;
+    this.calculado=true;
   }
 
   resta():void{
     this.result = this.num1 - this.num2;
+    this.calculado=true;
   }
 
   multiplicar():void{
     this.result = this.num1 * this.num2;
+    this.calculado=true;
   }
 
   dividir():void{
     if (this.num2 != 0) {
       this.result = this.num1/this.num2;
+      this.calculado=true;
     } else {
       alert("No se puede dividir por 0 ");
     }
